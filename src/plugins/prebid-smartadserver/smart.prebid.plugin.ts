90176eecda7060e9384807acb6b658c7445bde90
Douglas Rodrigues <douglas.rodrigues@webedia.local>
2017-07-17 11:09:37 -0300

    reverse-integral-ads_and_comscore

--- src/plugins/prebid-smartadserver/smart.prebid.plugin.ts
+++ src/plugins/prebid-smartadserver/smart.prebid.plugin.ts
@@ -27,61 +27,12 @@ export class SmartPrebidPlugIn implements PlugInInterface {
     
     return new Promise<void>(function(resolve, reject) {
 
-      function callIas(a){
-        var random = Math.floor(Math.random()*1000000000);
-
-        var integral = document.createElement('div');
-        integral.id = 'ias-'+random;
-
-        var el = document.getElementById(sas.info[a].divId);
-        el.appendChild(integral);
-
-        var iasScriptUrl, hiddenFrame, hiddenDoc, where, domain;
-        iasScriptUrl = '//pixel.adsafeprotected.com/jload?anId=922503&campId='+sas.info[a].creativeWidth+'x'+sas.info[a].creativeHeight+'&pubId='+sas.info[a].advertiserId+'&chanId='+sas.info[a].formatId+'&placementId='+sas.info[a].insertionId+'&pubCreative='+sas.info[a].creativeId+'&pubOrder='+sas.info[a].campaignId+'&cb='+random;
-        hiddenFrame = document.createElement('iframe');
-        (hiddenFrame.frameElement || hiddenFrame).style.cssText = "width: 0; height: 0; border: 0; display: none;";
-        hiddenFrame.src = 'javascript:false';
-        where = integral;
-        where.parentNode.insertBefore(hiddenFrame, where);
-        try {
-          hiddenDoc = hiddenFrame.contentWindow.document;
-        } catch (e) {
-          domain = document.domain;
-          hiddenFrame.src = "javascript:var d=document.open();d.domain='" + domain + "';void(0);";
-          hiddenDoc = hiddenFrame.contentWindow.document;
-        }
-        hiddenDoc.open().write('<body onload="' + 'window.__IntegralASUseFIF = true;' + 'var js = document.createElement(\'script\');' + 'js.src = \'' + iasScriptUrl + '\';' + 'document.body.appendChild(js);">');
-        hiddenDoc.close();
-      }
-
-      function callcomScore(a){
-        var random = Math.floor(Math.random()*1000000000);
-        var el = document.getElementById(sas.info[a].divId);  
-
-        if (/Android|webOS|iPhone|iPad|BlackBerry|Windows Phone|Opera Mini|IEMobile|Mobile/i.test(navigator.userAgent)) {
-          var comScorePixel = document.createElement("img");
-          comScorePixel.src = "http://b.scorecardresearch.com/p?c1=8&c2=6035191&c3="+options.siteId+"&ns_ap_it=b&rn="+random;
-          el.appendChild(comScorePixel);
-        } else {
-          var _comscore = _comscore || [];  
-          _comscore.push({ c1: "8", c2: "6035191", c3: options.siteId});      
-          (function() {
-            var s = document.createElement("script"); s.async = true;
-            s.src = (document.location.protocol == "https:" ? "https://sb" : "http://b") + ".scorecardresearch.com/beacon.js";
-            el.appendChild(s);
-          })();
-        }
-      }
-
       window['sasCallback'] = function(event) {
 
         Logger.logWithTime(sas.info[event].divId, 'finished slot rendering');
 
         let slot = self.slots[sas.info[event].divId];
         AutoRefresh.start(slot, options, self.autoRefresh);
-
-        callIas(event);
-        callcomScore(event);
 
         if (options.sasCallback)
           options.sasCallback(event);
@@ -212,6 +163,14 @@ private autoRefresh(slot: SmartAdSlot, options: any) {
   });
 }
 
+loadSlot(element: HTMLElement) {
+  var slot = new SmartAdSlot(element);
+
+  window._molotovAds.slots[element.id] = slot;
+
+  return slot;
+}
+
 private getSlots() {
   let slots = {};
 
@@ -219,10 +178,8 @@ private getSlots() {
     let el = window._molotovAds.slots[slot].HTMLElement;
 
     if(el.dataset.madAdunit === '') continue;
-
-    slots[el.id] = new SmartAdSlot(el);
-
-    window._molotovAds.slots[el.id] = slots[el.id];
+    
+    slots[el.id] = this.loadSlot(el);
   }
 
   return slots;
